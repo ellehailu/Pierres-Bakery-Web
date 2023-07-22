8724c278f7d91e5855bfedbf0f0edbd4cc118832
ellehailu <hailu.elle@gmail.com>
2023-07-22 16:00:33 -0700

    add an index page to View/Home and a home controller page

--- PierresBakery/Controllers/HomeController.cs
+++ PierresBakery/Controllers/HomeController.cs
@@ -4,6 +4,10 @@ namespace ProjectName.Controllers
 {
     public class HomeController : Controller
     {
-
+        [HttpGet("/")]
+        public ActionResult Index()
+        {
+            return View();
+        }
     }
 }
