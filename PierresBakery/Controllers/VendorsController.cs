2d543e3069a36a922c90dda7a95d6bddcf61bdec
ellehailu <hailu.elle@gmail.com>
2023-07-22 16:12:24 -0700

    added index page and new route for Vendors

--- PierresBakery/Controllers/VendorsController.cs
+++ PierresBakery/Controllers/VendorsController.cs
@@ -1,12 +1,17 @@
 using System.Collections.Generic;
 using System;
 using Microsoft.AspNetCore.Mvc;
-// using ProjectName.Models;
+using PierresBakery.Models;
 
 namespace PierresBakery.Controllers
 {
     public class VendorsController : Controller
     {
-
+        [HttpGet("/vendors")]
+        public ActionResult Index()
+        {
+            List<Vendors> allVendors = Vendors.GetAll();
+            return View(allVendors);
+        }
     }
 }
