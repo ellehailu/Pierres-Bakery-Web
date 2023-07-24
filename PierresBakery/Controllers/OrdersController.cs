f80ea7ddc39c4d4ae57eb6ab1bc5fd74f9fe2d6e
ellehailu <hailu.elle@gmail.com>
2023-07-23 22:07:31 -0700

    add an orders controller and index page

--- PierresBakery/Controllers/OrdersController.cs
+++ PierresBakery/Controllers/OrdersController.cs
@@ -3,7 +3,27 @@ using System;
 using Microsoft.AspNetCore.Mvc;
 using PierresBakery.Models;
 
-namespace PierresBakery.Controllers{
-    
+namespace PierresBakery.Controllers
+{
+    public class OrderController : Controller
+    {
+        [HttpGet("/vendors/{vendorsId}/orders/new")]
+        public ActionResult New(int vendorsId)
+        {
+            Vendors vendor = Vendors.Find(vendorsId);
+            return View(vendor);
+        }
+
+        [HttpGet("/vendors/{vendorsId}/orders/{orderId}")]
+        public ActionResult Show(int vendorsId, int orderId)
+        {
+            Order order = Order.Find(orderId);
+            Vendors vendor = Vendors.Find(vendorsId);
+            Dictionary<string, object> model = new Dictionary<string, object>();
+            model.Add("order", order);
+            model.Add("vendor s", vendor);
+            return View(model);
+        }
+    }
 }
 
